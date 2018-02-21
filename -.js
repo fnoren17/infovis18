babe5363c07bb9e6dc8a645af49306d4821d1de5
antmar <antmar@kth.se>
2018-02-21 11:39:15 +0100

    La till dummydata

---
+++ -.js
@@ -0,0 +1,27 @@
+var dataStructureDummy = [
+  {"Exporter": 
+    "Brazil"{
+      "Europe": {
+        "Sweden": {
+            "Soy": 1555,
+            "Rubber": 780,
+            "Plywood": 6012,
+            "Total_co2": 8347
+        },
+        "Norway": {
+            "Banana": 4030,
+            "Plywood": 444,
+            "Fire Ant": 311,
+            "Total_co2": 4785
+        }
+      },
+      "Africa": {
+        "Ghana": {
+            "Soy": 9523,
+            "Rubber": 514,
+            "Total_co2": 10037
+        }
+      }
+    }
+  }
+];
