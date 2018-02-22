ff34b9ac99e3cfc8d7851d13f1e4dfc6e471d340
antmar <antmar@kth.se>
2018-02-22 11:34:27 +0100

    Ändrat color scheme

--- handler.js
+++ handler.js
@@ -37,7 +37,7 @@
               console.log(d.properties.name)
               if (d.properties.name == "Brazil") {
                   console.log("hej brazil")
-                  return "fill: #248f8f";
+                  return "fill: #d8965d";
               }
           })
         .attr("d", path)
